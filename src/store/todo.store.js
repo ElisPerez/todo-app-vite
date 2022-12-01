0695e53f3609d96b55e96ca449a6aa7f51307db3
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-12-01 14:00:34 -0500

    store de To Do

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,6 +20,54 @@ const initStore = () => {
   console.log('initStore 🥑');
 };
 
+const loadStore = () => {
+  throw new Error('Not inplemented');
+};
+
+const getTodos = (filter = Filters.All) => {
+  
+};
+
+/**
+ * Add new ToDo
+ * @param {String} description
+ */
+const addTodo = description => {
+  throw new Error('Not inplemented');
+};
+
+/**
+ * Hacer toggle a la propiedad "this.done"
+ * Pending: false, Completed: true.
+ * @param todoId - The id of the todo to toggle
+ */
+const toggleTodo = todoId => {
+  throw new Error('Not inplemented');
+};
+
+const deleteTodo = todoId => {
+  throw new Error('Not inplemented');
+};
+
+const deleteCompleted = () => {
+  throw new Error('Not inplemented');
+};
+
+const setFilter = (newFilter = Filters.All) => {
+  throw new Error('Not inplemented');
+};
+
+const getCurrentFilter = () => {
+  throw new Error('Not inplemented');
+};
+
 export default {
+  addTodo,
+  deleteCompleted,
+  deleteTodo,
+  getCurrentFilter,
   initStore,
+  loadStore,
+  setFilter,
+  toggleTodo,
 };
